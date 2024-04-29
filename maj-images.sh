80db68cf612a36f2700143bf052f705d9a99447f
Thomas Guignard <tom@timtom.ch>
2024-04-29 12:59:02 -0400

    Interromp feh pendant la mise à jour

--- maj-images.sh
+++ maj-images.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-
+pkill feh
 rm /home/pi/Pictures/EcranAccueil/*
 /usr/local/bin/gdown --fuzzy 'https://drive.google.com/drive/folders/1QggHap8cRbgej-ZTIq37hQr04KKxYXyN?usp=sharing' -O /home/pi/Pictures/EcranAccueil --folder
+bash /home/pi/diaporama-accueil.sh
