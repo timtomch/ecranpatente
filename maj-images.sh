41dfef719f244696ff5a9d959985b83c4dfe0062
Thomas Guignard <tom@timtom.ch>
2026-01-04 11:22:52 -0500

    Go around gdown file limit
    Circumvent 50 file limit for gdown, which was preventing the update.

--- maj-images.sh
+++ maj-images.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 pkill feh
 rm /home/ecran/Pictures/EcranAccueil/*
-/home/ecran/gdown/bin/gdown --fuzzy 'https://drive.google.com/drive/folders/1QggHap8cRbgej-ZTIq37hQr04KKxYXyN?usp=sharing' -O /home/ecran/Pictures/EcranAccueil --folder
+/home/ecran/gdown/bin/gdown --remaining-ok --fuzzy 'https://drive.google.com/drive/folders/1QggHap8cRbgej-ZTIq37hQr04KKxYXyN?usp=sharing' -O /home/ecran/Pictures/EcranAccueil --folder
 bash /home/ecran/ecranpatente/diaporama-accueil.sh
