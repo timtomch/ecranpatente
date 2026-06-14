0631a6e75bc01a97bfba9e49526d63a5b8c9a2c2
Thomas Guignard <tom@timtom.ch>
2026-06-14 15:44:22 -0400

    Ajout affichage ruche

--- switchview.sh
+++ switchview.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-x=60
+x=30
 xdotool mousemove 3000 3000
-xdotool search --name Chromium windowactivate %@
+
+# Window 1
+xdotool search --name Calendrier windowactivate --sync %@
 xdotool key F5
 sleep $x
+
+# Window 2
+xdotool search --name Ruche windowactivate --sync %@
+xdotool key F5
+sleep $x
+
 xdotool search --name feh windowactivate %@
